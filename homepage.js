bc4eb837da3b616f5e77ca26b29b3d89b2e52958
Giorgi-Obgaidze <gobga17@freeuni.edu.ge>
2020-12-21 02:20:06 +0400

    login

---
+++ homepage.js
@@ -0,0 +1,9 @@
+
+
+function closeSignInForm(){
+    document.getElementById("sign-in").style.display = "none"
+}
+
+function openSignInForm(){
+    document.getElementById("sign-in").style.display = "block"
+}
