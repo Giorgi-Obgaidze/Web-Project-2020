aecc67ea10473e15eab4c1a2a7a934f21e3aa27a
Giorgi-Obgaidze <gobga17@freeuni.edu.ge>
2021-01-10 00:06:53 +0400

    sign in

--- header.js
+++ header.js
@@ -6,14 +6,43 @@ function closeSignInForm(){
 
 function openSignInForm(buttonId){
     if (String(buttonId).localeCompare("sign-in") == 0) {
-        document.getElementById("title").innerHTML = "Sign In";
+        document.getElementById("title").innerHTML = "Sign In"
         document.getElementById("username").style.display = "none"
         document.getElementById("usernameLabel").style.display = "none"
         document.getElementById("sign-in").style.display = "block"
+        document.getElementById("submit_but").innerHTML = "Sign In"
+        document.getElementById("submit_but").onclick = signInFunction
     }else if (String(buttonId).localeCompare("sign-up") == 0){
-        document.getElementById("title").innerHTML = "Sign Up";
+        document.getElementById("title").innerHTML = "Sign Up"
         document.getElementById("username").style.display = "block"
         document.getElementById("usernameLabel").style.display = "block"
         document.getElementById("sign-in").style.display = "block"
+        document.getElementById("submit_but").innerHTML = "Sign Up"
+        document.getElementById("submit_but").onclick = signUpFunction
     }
 }
+
+function signInFunction(){
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+
+    if(email != "" && password != ""){
+        var trySignIn = firebase.auth().signInWithEmailAndPassword(email, password)
+
+        trySignIn.catch(function(error){
+            var warning = error.code
+            var errorMessage = error.message
+            console.log(warning)
+            window.alert(errorMessage)
+        });
+        console.log("login test")
+    }else{
+        window.alert("Please fill out all fields.")
+    }
+}
+
+function signUpFunction(){
+    console.log("sign up")
+}
+
+
---
+++ index.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyATkOngB-vguO3gW_rRZzjoU3aqUXGehL4",
+    authDomain: "web-project-2020-eaf1c.firebaseapp.com",
+    projectId: "web-project-2020-eaf1c",
+    storageBucket: "web-project-2020-eaf1c.appspot.com",
+    messagingSenderId: "1089753996768",
+    appId: "1:1089753996768:web:bb61d14d9ec6af122c5f85"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.auth.Auth.Persistence.LOCAL;
